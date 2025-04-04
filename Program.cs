82bc8b42e04ad40b89730ea786c1b5b0074a8b8a
sunaykazimbeylii <sunaysk-bb209@code.edu.az>
2025-04-04 12:31:09 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using InterfaceStatic.Models;
+
+namespace InterfaceStatic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Sunay";
+            string surname = "Kazimbeyli";
+            if (Student.CheckName(name) && Student.CheckName(surname))
+            {
+                Student student = new Student(name, surname);
+                Console.WriteLine($"Name:{name} Surname:{surname}");
+
+                Console.WriteLine(student.GenerateEmail());
+                Console.WriteLine();
+
+            }
+            else
+            {
+                Console.WriteLine("duzgun daxil et");
+            }
+
+
+
+        }
+    }
+}
